f023ae55d5c8f6837ebffa32f1748b77fc3d42b7
farstadb <48350617+farstadb@users.noreply.github.com>
2019-03-08 12:17:18 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Search</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+    <form action="dbconnections/search.php" method="GET">
+        <input type="text" name="query" />
+        <input type="submit" value="Search" />
+    </form>
+</body>
+</html>
