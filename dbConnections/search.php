bb1ae7c62632e4313cf7474ae216eb4418aea884
farstadb <48350617+farstadb@users.noreply.github.com>
2019-03-12 10:29:46 +0100

    Forside oppdatering
    Forside og søkemotor front end endret. lagt til video

--- dbConnections/search.php
+++ dbConnections/search.php
@@ -29,7 +29,7 @@ function CloseCon($conn)
     $query = $_GET['query']; 
     // gets value sent over search form
      
-    $min_length = 3;
+    $min_length = 1;
     // you can set minimum length of the query if you want
      
     if(strlen($query) >= $min_length){ // if query length is more or equal minimum length then
@@ -43,11 +43,11 @@ function CloseCon($conn)
         $raw_results = mysqli_query(openCon(), "SELECT * FROM bugs
             WHERE bugname LIKE '%".$query."%' "); //or die(mysql_error());
              
-        // * means that it selects all fields, you can also write: `id`, `title`, `text`
-        // articles is the name of our table
+        // * means that it selects all fields
+        // bugs is the name of our table
          
         // '%$query%' is what we're looking for, % means anything, for example if $query is Hello
-        // it will match "hello", "Hello man", "gogohello", if you want exact match use `title`='$query'
+        // it will match "hello", "Hello man", "gogohello", if you want exact match use `bugname`='$query'
         // or if you want to match just full word so "gogohello" is out use '% $query %' ...OR ... '$query %' ... OR ... '% $query'
          
         if(mysqli_num_rows($raw_results) > 0){ // if one or more rows are returned do following
@@ -56,7 +56,7 @@ function CloseCon($conn)
             // $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
              
                 echo "<p><h3>".$results['bugname']."</h3>".$results['bugDescription'].$results['InStock']."</p>";
-                // posts results gotten from database(title and text) you can also show id ($results['id'])
+                // posts results gotten from database(bugname and bugdescription) you can also show id ($results['id'])
             }
              
         }
@@ -66,7 +66,7 @@ function CloseCon($conn)
          
     }
     else{ // if query length is less than minimum
-        echo "Minimum length is ".$min_length;
+        echo "No results";
     } // test
 ?>
 </body>
--- index.php
+++ index.php
@@ -1,14 +1,50 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Search</title>
+    <title>How Many Bugs Can you find?</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link rel="stylesheet" type="text/css" href="style.css"/>
+    <style type="text/css">
+    	body {
+    		font-family: Helvetica;
+    	}
+    	#myVideo {
+  			position: absolute;
+  			right: 0;
+  			bottom: 0;
+  			min-width: 100%; 
+  			min-height: 100%;
+		}
+		#boks {
+			height: 50px;
+			width: 550px;
+			/*background-color: rgb(150,150,150);*/
+			position: absolute;
+			text-align:  center;
+			top: 450px;
+			left: 590px;
+		}
+		input[type=text], select {
+  			width: 100%;
+  			padding: 12px 20px;
+  			margin: 8px 0;
+  			display: inline-block;
+  			border: 1px solid #ccc;
+  			border-radius: 4px;
+  			box-sizing: border-box;
+		
+
+    </style>
 </head>
 <body>
-    <form action="dbconnections/search.php" method="GET">
-        <input type="text" name="query" />
-        <input type="submit" value="Search" />
-    </form>
+<video autoplay muted loop id="myVideo">
+  <source src="video/HowManyBugs2.mp4" type="video/mp4">
+</video>
+		<div id ="boks">
+    		<form action="dbconnections/search.php" method="GET">
+        		<input type="text" name="query" placeholder="Search for bugs.." />        		
+    		</form>
+    	</div>
+    		
 </body>
 </html>
