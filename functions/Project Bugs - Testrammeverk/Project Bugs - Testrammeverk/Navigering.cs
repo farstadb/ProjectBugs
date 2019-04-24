e992303d9961feff73724222de839ecc26dda1d2
johbrobert <48353469+johbrobert@users.noreply.github.com>
2019-04-24 14:03:57 +0200

    Testautomatiseringsrammeverk
    Rammeverk med alle funksjonene til Project Bugs nettsiden.

---
+++ functions/Project Bugs - Testrammeverk/Project Bugs - Testrammeverk/Navigering.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+
+namespace Project_Bugs___Testrammeverk
+{
+    class Navigering
+    {
+        public static void LastHovedside()
+        {
+            Variabler.nettleser.Url = Variabler.URL;
+        }
+
+        public static void LogoTrykk()
+        {
+            Variabler.nettleser.FindElement(By.XPath("//img")).Click();
+        }
+
+        public static void VisAlt()
+        {
+            Variabler.nettleser.FindElement(By.CssSelector(".showAll")).Click();
+        }
+
+        public static void GåTilOpprettBruker()
+        {
+            Variabler.nettleser.FindElement(By.CssSelector(".oppretteBrukerKnapp")).Click();
+        }
+    }
+}
---
+++ functions/Project Bugs - Testrammeverk/Project Bugs - Testrammeverk/Variabler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+
+namespace Project_Bugs___Testrammeverk
+{
+    class Variabler
+    {
+        public static string URL = "http://localhost:8080/ProjectBugs/index.php";
+        public static IWebDriver nettleser;
+    }
+}
