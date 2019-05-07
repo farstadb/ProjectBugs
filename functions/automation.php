36c7001d35c4e40ef08f65dbbed24b6bedc800f2
johbrobert <48353469+johbrobert@users.noreply.github.com>
2019-05-07 11:58:59 +0200

    Update automation.php

--- functions/automation.php
+++ functions/automation.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<title>Test Data Management</title>
+<title>Test Automation</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -48,13 +48,10 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
   <h4 class="w3-bar-item"><b>Menu</b></h4>
   <a class="w3-bar-item w3-button w3-hover-black" href="#top">Introduction</a>
   <a class="w3-bar-item w3-button w3-hover-black" href="#Info">What is Automation</a>
-  <a class="w3-bar-item w3-button w3-hover-black" href="#What">Different types of automation</a>
-  <a class="w3-bar-item w3-button w3-hover-black" href="#Importing">Automation tools</a>
-  <a class="w3-bar-item w3-button w3-hover-black" href="#Assignment1" span style="margin-left:2em">Selenium</a>
-  <a class="w3-bar-item w3-button w3-hover-black" href="#Assignment1">Assignment 1</a>
   <br></br>
-  <a class="w3-bar-item w3-button w3-hover-black" href="http://localhost:8080/projectbugs/functions/seleniumSetup.php">[Setting up Selenium]</a>
-  <a class="w3-bar-item w3-button w3-hover-black" href="http://localhost:8080/projectbugs/functions/seleniumScript.php">[Writing Selenium Scripts]</a>
+  <a class="w3-bar-item w3-button w3-hover-black" href="http://localhost:8080/projectbugs/functions/seleniumSetup.php">Selenium</a>
+  <a class="w3-bar-item w3-button w3-hover-black" href="http://localhost:8080/projectbugs/functions/seleniumScript.php">UiPath</a>
+  <a class="w3-bar-item w3-button w3-hover-black" href="http://localhost:8080/projectbugs/functions/seleniumScript.php">TestComplete</a>
   <br></br><a class="w3-bar-item w3-button w3-hover-black" href="http://localhost:8080/projectbugs/index.php"target="_blank">Go to test site</a>
 </nav>
 
@@ -76,49 +73,21 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
     <div class="w3-twothird w3-container">
       <h2 class="w3-text-teal"id ="Info">Introduction</h2>
       <p>
-
+        When testing, there are often repetitive and time consuming tests that need to be performed, as well as tests that aren't feasible to do manually. By automating, one can greatly reduce the time and effort required to run a test.
       </p>
     </div>
 
   <div class="w3-row">
     <div class="w3-twothird w3-container">
-      <h2 class="w3-text-teal" id ="What">What is Automation</h2>
-      <p></p>
-    </div>
-
-  <div class="w3-row w3-padding-64">
-    <div class="w3-twothird w3-container">
-      <h2 class="w3-text-teal"id ="Generating">Different types of automations</h2>
+      <h2 class="w3-text-teal" id ="What">What is Test Automation?</h2>
       <p>
+        Test Automation means using an automation tool to run tests. When a user performs a test, they have to do do every step in the test process manually. With test automation, the tests can be run from start to end automatically, requiring no input from the user. This can be especially when testing the same area over and over.
 
+        <br><br>From the Training Arena you will you will learn to use several test automation tools, like Selenium, UiPath and TestComplete.
       </p>
     </div>
 
-    <div class="w3-row w3-padding-64">
-    <div class="w3-twothird w3-container">
-      <h2 class="w3-text-teal"id ="Importing">Automation tools</h2>
-      <p>
-    </p>
-    </div>
 
-    <div class="w3-row w3-padding-64">
-    <div class="w3-twothird w3-container">
-      <h3 class="w3-text-teal">Selenium</h3>
-      <p>....</p>
-    </div>
-
-    <div class="w3-row w3-padding-64">
-    <div class="w3-twothird w3-container">
-      <h2 class="w3-text-teal"id ="Assignment2">Assignment 1: </h2>
-      <p></p>
-    </div>
-
-    <div class="w3-row w3-padding-64">
-    <div class="w3-twothird w3-container">
-      <h3 class="w3-text-teal">How to do it yourself</h3>
-      <p>......</p>
-      <p><a href="#top">Top</a></p>
-    </div>
 
 
 <!-- END MAIN -->
