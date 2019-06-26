e9579980913480ede03931b0441a5d64310ab245
johbrobert <48353469+johbrobert@users.noreply.github.com>
2019-06-26 10:38:19 +0200

    Update generatingLink.php
    Updated format of Generating test data page

--- functions/generatingLink.php
+++ functions/generatingLink.php
@@ -47,11 +47,12 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
   <a class="w3-bar-item w3-button w3-hover-black" href="http://localhost:8080/projectbugs/functions/testManagement.php">Back to Test Data Management</a>
 
   <br></br>
-  <a class="w3-bar-item w3-button w3-hover-black" href="http://localhost:8080/projectbugs/functions/generatingLink.php">Test-Generation</a>
-  <a class="w3-bar-item w3-button w3-hover-black" span style="margin-left:2em" href="#top">Introduction</a>
-  <a class="w3-bar-item w3-button w3-hover-black" span style="margin-left:2em" href="#Info">What is generating</a>
-  <a class="w3-bar-item w3-button w3-hover-black" span style="margin-left:2em" href="#What">How to do it</a>
-  <a class="w3-bar-item w3-button w3-hover-black" span style="margin-left:2em" href="#Assignment1">Assignment 1</a> <br>
+  <a class="w3-bar-item w3-button w3-hover-black" href="#Headline">Test-Generation</a>
+  <a class="w3-bar-item w3-button w3-hover-black" span style="margin-left:2em" href="#skill">Technical skill level</a>
+  <a class="w3-bar-item w3-button w3-hover-black" span style="margin-left:2em" href="#Info">What is test data generation</a>
+  <a class="w3-bar-item w3-button w3-hover-black" span style="margin-left:2em" href="#Generate">How to do it yourself</a>
+  <a class="w3-bar-item w3-button w3-hover-black" span style="margin-left:2em" href="#Assignment1">Exercise</a>
+    <a class="w3-bar-item w3-button w3-hover-black" span style="margin-left:2em" href="#help">Help!</a> <br>
   <a class="w3-bar-item w3-button w3-hover-black" href="http://localhost:8080/projectbugs/functions/populatingLink.php">Test-Population</a>
   <br><a class="w3-bar-item w3-button w3-hover-black" href="http://localhost:8080/projectbugs/index.php"target="_blank">Go to test site</a>
 </nav>
@@ -64,26 +65,29 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
   <div class="w3-row w3-padding-64">
     <div class="w3-twothird w3-container">
     	<br></br>
-      <h1 class="w3-text-teal"id ="Headline">Get started with Test Data Generation</h1>
+      <h1 class="w3-text-teal"id ="Headline">Test Data Generation</h1>
       <p>
+        Welcome to Test Data Generation. Here you will learn how to generate data, and how to set up a tool for this. There will also be an exercise at the end.
       </p>
     </div>
 
     <div class="w3-row w3-padding-64">
     <div class="w3-twothird w3-container">
-      <h2 class="w3-text-teal"id ="Info">Introduction</h2>
-      <p> Welcome to Test Data Generation. Here you will learn how to generate data, and how to set up a tool for this. There will also be an assignment for this. 
+      <h3 class="w3-text-teal"id ="skill">Technical skill level</h3>
+      <p>Intermediate.<br>
+      Estimated time: 30 minutes</p> 
     </div>
 
-  <div class="w3-row">
+    <div class="w3-row w3-padding-64">
     <div class="w3-twothird w3-container">
-      <h2 class="w3-text-teal" id ="What">What is generating</h2>
-      <p>When applying Test Data Generation, you find a tool that allows you to generate a high number of users, in a short period of time. This will lead to higher quality of testing, and is easier to use, than to type in a high number of users manually.</p>
+      <h3 class="w3-text-teal"id ="Info">What is Test Data Generation?</h3>
+      <p>
+      When applying Test Data Generation, you find a tool that allows you to generate a high number of users, in a short period of time. This will lead to higher quality of testing, and is easier to use, than to type in a high number of users manually.</p>
     </div>
 
   <div class="w3-row w3-padding-64">
     <div class="w3-twothird w3-container">
-      <h2 class="w3-text-teal"id ="Generate">How to do it</h2>
+      <h3 class="w3-text-teal"id ="Generate">How to do it yourself</h3>
       <p>One good tool to use for this is: Mockaroo
       Mockaroo lets you generate up to 1,000 rows of realistic test data in CSV, JSON, SQL, and Excel formats. 
       <a href = "http://mockaroo.com/" target="_blank" >URL: https://mockaroo.com/ (Does not need installation).</a>
@@ -102,11 +106,11 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
 
     <div class="w3-row w3-padding-64">
     <div class="w3-twothird w3-container">
-      <h2 class="w3-text-teal"id ="Assignment1">Assignment 1: Generate Test Data</h2>
+      <h3 class="w3-text-teal"id ="Assignment1">Exercise</h3>
       <p>
         Now that you have learned how to generate test data from a website, it is time to use it to generate data. This data will later be used to populate a database in Project Bugs.  <br><br>
 
-        The assignment is as follows:<br><br>
+        The exercise is as follows:<br><br>
 
         Generate Test Data by using the following website: <a href = "http://mockaroo.com/" target="_blank" >https://mockaroo.com/.</a><br>
       <br>
@@ -117,9 +121,19 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
       <br> - Gender
       <br> - Phonenumber
       <br> - Password. <br>
-      <br> Generate 500 rows, and save it in Excel-format. Save this for use later in assignment 2 - Test-Population.
+      <br> Generate 500 rows, and save it in Excel-format. Save the Excel document for later use in the Populate Test Data exercise.
+      </p>
+    </div>
+
+        <div class="w3-row w3-padding-64">
+    <div class="w3-twothird w3-container">
+      <h3 class="w3-text-teal"id ="help">Help!</h3>
+      <p>
+        Contact John Robert Ursin Bakk if you require help.<br>
+        tlf: 98626683<br>
+        Mail: john-robert.bakk@sogeti.no
       </p><br>
-        	<p><a href="#top">Go back to the top</a></p>
+          <p><a href="#top">Go back to the top</a></p>
     </div>
     <!-- END MAIN -->
 </div>
