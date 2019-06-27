3694cc3da3525fb69d8a78c9ef426b2fb5f5cf8d
cmartono <charles.martono@sogeti.no>
2019-06-27 13:17:16 +0200

    Update testManagement.php
    Modifications:
    Change menu system and content on "Test Data management" section.

--- functions/testManagement.php
+++ functions/testManagement.php
@@ -47,8 +47,8 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
   </a>
   <h4 class="w3-bar-item"><b>Menu</b></h4>
 
-  <a class="w3-bar-item w3-button w3-hover-black" href="http://localhost:8080/projectbugs/functions/generatingLink.php">Go to Test Data Generation</a>
-  <a class="w3-bar-item w3-button w3-hover-black" href="http://localhost:8080/projectbugs/functions/populatingLink.php">Go to Test Data Population</a>
+  <a class="w3-bar-item w3-button w3-hover-black" href="http://localhost:8080/projectbugs/functions/generatingLink.php">Test Data Generation</a>
+  <a class="w3-bar-item w3-button w3-hover-black" href="http://localhost:8080/projectbugs/functions/populatingLink.php">Test Data Population</a>
   <br></br><a class="w3-bar-item w3-button w3-hover-black" href="http://localhost:8080/projectbugs/index.php"target="_blank">Go to test site</a>
 </nav>
 
@@ -97,7 +97,9 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
               <div class="w3-row">
     <div class="w3-twothird w3-container">
       <h3 class="w3-text-teal" id ="Who">Why not copy production data for tests?</h3>
-      <p>Due to data security and regulatory matter, production data is often not practical for use in a test system. Data that has personally identifiable information must be altered in order to protect people from having sensitive data exposed to the development and testing teams. Test data management uses data masking techniques to obfuscate personally identifiable information while still retaining the formatting and other data properties that are important for testing.
+      <p>Due to data security and regulatory matter, production data is often not practical for use in a test system. Data that has personally identifiable information must be altered in order to protect people from having sensitive data exposed to the development and testing teams. Test data management uses data masking techniques to obfuscate personally identifiable information while still retaining the formatting and other data properties that are important for testing.<br>
+      <br>
+      In this section, you will learn how to generate test data, as well as how to use the test data to populate a website. See the respective pages on the side bar to learn more.
       </p><br>
           <p><a href="#top">Go back to the top</a></p>
     </div>
